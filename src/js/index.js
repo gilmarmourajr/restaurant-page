f583f2ce4e3ca27b0aad585c3ceab9cd9f808c9c
gilmarmourajr <juzinho-gilmar@hotmail.com>
2025-04-21 00:40:53 -0300

    Add switch page functionality

--- src/js/index.js
+++ src/js/index.js
@@ -3,4 +3,26 @@ import homeLoad from "./home.js";
 import menuLoad from "./menu.js";
 import contactLoad from "./contact.js";
 
-contactLoad();
+homeLoad();
+eventListeners();
+
+function clear () {
+    document.getElementById("content").innerHTML = '';
+}
+
+function eventListeners () {
+    document.getElementById("homeBtn").addEventListener("click", () => {
+        clear();
+        homeLoad();
+    });
+
+    document.getElementById("menuBtn").addEventListener("click", () => {
+        clear();
+        menuLoad();
+    });
+
+    document.getElementById("contactBtn").addEventListener("click", () => {
+        clear();
+        contactLoad();
+    })
+}
